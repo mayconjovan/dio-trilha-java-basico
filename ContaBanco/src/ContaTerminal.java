7ea1df6975f8af572dc68df88ee4b5281d3a9bc4
Maycon J Pereira <maycon.jovan@petzpartner.com.br>
2023-04-23 17:26:22 -0300

    Exercício resolvido

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,31 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws Exception {    
+
+        int conta;
+        String agencia;
+        String nome;
+        double saldo;
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Bem vindo ao DIOBank!");
+        System.out.println("Por favor, insira o numero da sua conta.");
+        conta = scanner.nextInt();
+
+        System.out.println("Agora insira o número da agência com hífen.");
+        agencia = scanner.next();
+
+        System.out.println("Agora informe o seu nome.");
+        nome = scanner.next();
+
+        System.out.println("Informe o saldo desejado.");
+        saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia +
+                ", conta: " + conta + " e seu saldo de: R$" + saldo + " já está disponível para saque.");
+
     }
 }
